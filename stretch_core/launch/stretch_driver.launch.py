194943408b070f4bc8f1251efbe2dd2e6ae04c44
David V. Lu <davidvlu@gmail.com>
2021-10-11 11:57:43 -0400

    PR Feedback

---
+++ stretch_core/launch/stretch_driver.launch.py
@@ -0,0 +1,81 @@
+from ament_index_python.packages import get_package_share_path
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command, LaunchConfiguration
+
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    stretch_core_path = get_package_share_path('stretch_core')
+
+    declare_broadcast_odom_tf_arg = DeclareLaunchArgument(
+        'broadcast_odom_tf',
+        default_value='False', choices=['True', 'False'],
+        description='Whether to broadcast the odom TF'
+    )
+
+    declare_fail_out_of_range_goal_arg = DeclareLaunchArgument(
+        'fail_out_of_range_goal',
+        default_value='True', choices=['True', 'False'],
+        description='Whether the motion action servers fail on out-of-range commands'
+    )
+
+    declare_mode_arg = DeclareLaunchArgument(
+        'mode',
+        default_value='position', choices=['position', 'navigation', 'calibration'],
+        description='The mode in which the ROS driver commands the robot'
+    )
+
+    declare_controller_arg = DeclareLaunchArgument(
+        'calibrated_controller_yaml_file',
+        default_value=str(stretch_core_path / 'config' / 'controller_calibration_head.yaml'),
+        description='Path to the calibrated controller args file'
+    )
+
+    robot_description_content = Command(['xacro ',
+                                         str(get_package_share_path('stretch_description') / 'urdf' / 'stretch.urdf')])
+
+    joint_state_publisher = Node(package='joint_state_publisher',
+                                 executable='joint_state_publisher',
+                                 output='log',
+                                 parameters=[{'source_list': ['/stretch/joint_states']},
+                                             {'rate': 15}])
+
+    robot_state_publisher = Node(package='robot_state_publisher',
+                                 executable='robot_state_publisher',
+                                 output='both',
+                                 parameters=[{'robot_description': robot_description_content},
+                                             {'publish_frequency': 15.0}])
+
+    aggregator = Node(package='diagnostic_aggregator',
+                      executable='aggregator_node',
+                      output='log',
+                      parameters=[str(stretch_core_path / 'config/diagnostics.yaml')])
+
+    stretch_driver_params = [
+        {'rate': 25.0,
+         'timeout': 0.5,
+         'controller_calibration_file': LaunchConfiguration('calibrated_controller_yaml_file'),
+         'broadcast_odom_tf': LaunchConfiguration('broadcast_odom_tf'),
+         'fail_out_of_range_goal': LaunchConfiguration('fail_out_of_range_goal'),
+         'mode': LaunchConfiguration('mode')}
+    ]
+
+    stretch_driver = Node(package='stretch_core',
+                          executable='stretch_driver',
+                          emulate_tty=True,
+                          output='screen',
+                          remappings=[('cmd_vel', '/stretch/cmd_vel'),
+                                      ('joint_states', '/stretch/joint_states')],
+                          parameters=stretch_driver_params)
+
+    return LaunchDescription([declare_broadcast_odom_tf_arg,
+                              declare_fail_out_of_range_goal_arg,
+                              declare_mode_arg,
+                              declare_controller_arg,
+                              joint_state_publisher,
+                              robot_state_publisher,
+                              aggregator,
+                              stretch_driver])
--- stretch_core/setup.py
+++ stretch_core/setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_packages
+from glob import glob
 
 package_name = 'stretch_core'
 
@@ -9,10 +10,12 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
+        ('share/' + package_name + '/config', glob('config/*')),
     ],
     install_requires=['setuptools'],
-    url='',
-    license='',
+    url='https://github.com/hello-robot/stretch_ros2',
+    license='Apache License 2.0',
     author='Hello Robot Inc.',
     author_email='support@hello-robot.com',
     description='The stretch_core package',
