a79bb992e74f0894a28ef69fe897e9347be94fdc
hello-binit <64861565+hello-binit@users.noreply.github.com>
2021-10-15 07:03:02 -0400

    Add ros2_control backed fake controller driver launch file

---
+++ stretch_moveit2/README.md
@@ -0,0 +1,20 @@
+![](../images/banner.png)
+
+## Overview
+
+*stretch_moveit2* configures MoveIt2 for Stretch RE1. The MoveIt Motion Planning Framework makes manipulation, 3D perception, control/navigation, and more available on Stretch.
+
+
+## Running Drag & Drop Motion Planning Demo
+
+### On Stretch RE1
+
+TBD
+
+### On Fake Hardware
+
+TBD
+
+## License
+
+For license information, please see the LICENSE files.
---
+++ stretch_moveit2/launch/fake_driver.launch.py
@@ -0,0 +1,67 @@
+from ament_index_python.packages import get_package_share_path
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
+from launch.substitutions import Command, LaunchConfiguration
+
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    stretch_moveit_config_path = get_package_share_path('stretch_moveit2')
+    stretch_description_path = get_package_share_path('stretch_description')
+
+    # Load ROS2 Control backed XACRO and YAML
+    fake_description_content = Command(['xacro ',
+                                         str(stretch_moveit_config_path / 'urdf' / 'fake_description.xacro')])
+    fake_description = {'robot_description': fake_description_content}
+    fake_ros2_controllers = str(stretch_moveit_config_path / 'config' / 'fake_ros2_controllers.yaml')
+
+    # Launch args
+    declare_rviz_arg = DeclareLaunchArgument(
+        'rviz',
+        default_value='False', choices=['True', 'False'],
+        description='Whether to show Rviz'
+    )
+
+    declare_rviz_cfg_fpath_arg = DeclareLaunchArgument(
+        'rviz_cfg_fpath',
+        default_value=str(stretch_description_path / 'rviz' / 'stretch.rviz'),
+        description='Full path to the RVIZ config file to use'
+    )
+
+    # Nodes
+    rviz_node = Node(package='rviz2',
+                     executable='rviz2',
+                     arguments=['-d', LaunchConfiguration('rviz_cfg_fpath')],
+                     condition=IfCondition(LaunchConfiguration("rviz")))
+
+    static_odom_tf = Node(package='tf2_ros',
+                          executable='static_transform_publisher',
+                          arguments=['0.0', '0.0', '0.0', '0.0', '0.0', '0.0', 'odom', 'base_link'])
+
+    fake_state_publisher_node = Node(package='robot_state_publisher',
+                                      executable='robot_state_publisher',
+                                      parameters=[fake_description])
+
+    controller_manager_node = Node(package='controller_manager',
+                                   executable='ros2_control_node',
+                                   parameters=[fake_description, fake_ros2_controllers])
+
+    spawn_jsb_node = Node(package='controller_manager',
+                          executable='spawner',
+                          arguments=['joint_state_broadcaster'])
+
+    spawn_sc_node = Node(package='controller_manager',
+                          executable='spawner',
+                          arguments=['stretch_controller'])
+
+    return LaunchDescription([declare_rviz_arg,
+                              declare_rviz_cfg_fpath_arg,
+                              rviz_node,
+                              static_odom_tf,
+                              fake_state_publisher_node,
+                              controller_manager_node,
+                              spawn_jsb_node,
+                              spawn_sc_node])
