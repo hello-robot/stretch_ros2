00e7f2b6761d7c804527392192f8bd9ede7c5a97
Binit Shah <bshah@hello-robot.com>
2021-10-18 03:00:47 -0700

    Action server saves pickled goals automatically

--- stretch_core/stretch_core/joint_trajectory_server.py
+++ stretch_core/stretch_core/joint_trajectory_server.py
@@ -1,5 +1,9 @@
 #! /usr/bin/env python3
 
+import pickle
+from pathlib import Path
+import stretch_body.hello_utils as hu
+
 from rclpy.node import Node
 from rclpy.action import ActionServer
 
@@ -14,10 +18,18 @@ class JointTrajectoryAction(Node):
         self.action_server_rate = self.node.create_rate(action_server_rate_hz)
         self.server = ActionServer(self.node, FollowJointTrajectory, '/stretch_controller/follow_joint_trajectory',
                                    self.execute_cb)
+        self.debug_dir = Path(hu.get_stretch_directory('goals'))
+        if not self.debug_dir.exists():
+            self.debug_dir.mkdir()
 
     def execute_cb(self, goal_handle):
         if self.node.robot_mode != 'manipulation':
             self.node.get_logger().warn('Only manipulation mode support currently. Use /switch_to_manipulation_mode service.')
+
+        # save goal to log directory
+        goal_fpath = self.debug_dir / f'goal_{hu.create_time_string()}.pickle'
+        with goal_fpath.open('wb') as s:
+            pickle.dump(goal_handle.request, s)
 
         return self.success_callback(goal_handle, 'nothing happened')
 
