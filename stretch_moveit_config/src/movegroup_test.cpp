355f73100925ba8f896a974b23ae222e1be2ec76
hello-chintan <cdesai@hello-robot.com>
2022-03-23 16:30:56 -0400

    Visualize ee link planned trajectory in rviz

--- stretch_moveit_config/src/movegroup_test.cpp
+++ stretch_moveit_config/src/movegroup_test.cpp
@@ -27,20 +27,16 @@ int main(int argc, char** argv)
   executor.add_node(move_group_node);
   std::thread([&executor]() { executor.spin(); }).detach();
 
-  // BEGIN_TUTORIAL
-  //
   // Setup
-  // ^^^^^
-  //
   // MoveIt operates on sets of joints called "planning groups" and stores them in an object called
   // the ``JointModelGroup``. Throughout MoveIt, the terms "planning group" and "joint model group"
   // are used interchangeably.
-  static const std::string PLANNING_GROUP = "stretch_arm";
+  static const std::string PLANNING_GROUP_BASE_ARM = "mobile_base_arm";
 
   // The
   // :moveit_codedir:`MoveGroupInterface<moveit_ros/planning_interface/move_group_interface/include/moveit/move_group_interface/move_group_interface.h>`
   // class can be easily set up using just the name of the planning group you would like to control and plan for.
-  moveit::planning_interface::MoveGroupInterface move_group(move_group_node, PLANNING_GROUP);
+  moveit::planning_interface::MoveGroupInterface move_group(move_group_node, PLANNING_GROUP_BASE_ARM);
 
   // We will use the
   // :moveit_codedir:`PlanningSceneInterface<moveit_ros/planning_interface/planning_scene_interface/include/moveit/planning_scene_interface/planning_scene_interface.h>`
@@ -49,32 +45,33 @@ int main(int argc, char** argv)
 
   // Raw pointers are frequently used to refer to the planning group for improved performance.
   const moveit::core::JointModelGroup* joint_model_group =
-      move_group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
+      move_group.getCurrentState()->getJointModelGroup(PLANNING_GROUP_BASE_ARM);
+  const moveit::core::LinkModel* ee_parent_link = 
+      move_group.getCurrentState()->getLinkModel("link_grasp_center"); // the link name can be changed here to visualize the trajectory of the corresponding link
 
   // Visualization
-  // ^^^^^^^^^^^^^
   namespace rvt = rviz_visual_tools;
-//   moveit_visual_tools::MoveItVisualTools visual_tools(move_group_node, "panda_link0", "move_group_tutorial",
-//                                                       move_group.getRobotModel());
-  moveit_visual_tools::MoveItVisualTools visual_tools(move_group_node, "odom", "move_group_tutorial",
+  moveit_visual_tools::MoveItVisualTools visual_tools(move_group_node, "odom", "move_group_visualization",
                                                       move_group.getRobotModel());
   visual_tools.deleteAllMarkers();
 
   /* Remote control is an introspection tool that allows users to step through a high level script */
   /* via buttons and keyboard shortcuts in RViz */
   visual_tools.loadRemoteControl();
+  
+  // Start the demo
+  visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to start the demo");
+  
+  // Batch publishing is used to reduce the number of messages being sent to RViz for large visualizations
+  visual_tools.trigger();
 
   // RViz provides many types of markers, in this demo we will use text, cylinders, and spheres
   Eigen::Isometry3d text_pose = Eigen::Isometry3d::Identity();
   text_pose.translation().z() = 1.0;
   visual_tools.publishText(text_pose, "MoveGroupInterface_Demo", rvt::WHITE, rvt::XLARGE);
-
-  // Batch publishing is used to reduce the number of messages being sent to RViz for large visualizations
   visual_tools.trigger();
 
   // Getting Basic Information
-  // ^^^^^^^^^^^^^^^^^^^^^^^^^
-  //
   // We can print the name of the reference frame for this robot.
   RCLCPP_INFO(LOGGER, "Planning frame: %s", move_group.getPlanningFrame().c_str());
 
@@ -86,34 +83,10 @@ int main(int argc, char** argv)
   std::copy(move_group.getJointModelGroupNames().begin(), move_group.getJointModelGroupNames().end(),
             std::ostream_iterator<std::string>(std::cout, ", "));
 
-    // Start the demo
-  // ^^^^^^^^^^^^^^^^^^^^^^^^^
-  visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to start the demo");
-  
   // Now, we call the planner to compute the plan and visualize it.
-  // Note that we are just planning, not asking move_group
-  // to actually move the robot.
   moveit::planning_interface::MoveGroupInterface::Plan my_plan;
 
-  bool success = (move_group.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
-
-  // Moving to a pose goal
-  // ^^^^^^^^^^^^^^^^^^^^^
-  //
-  // Moving to a pose goal is similar to the step above
-  // except we now use the ``move()`` function. Note that
-  // the pose goal we had set earlier is still active
-  // and so the robot will try to move to that goal. We will
-  // not use that function in this tutorial since it is
-  // a blocking function and requires a controller to be active
-  // and report success on execution of a trajectory.
-
-  /* Uncomment below line when working with a real robot */
-  /* move_group.move(); */
-
   // Planning to a joint-space goal
-  // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  //
   // Let's set a joint space goal and move towards it.  This will replace the
   // pose target we set above.
   //
@@ -125,28 +98,33 @@ int main(int argc, char** argv)
   std::vector<double> joint_group_positions;
   current_state->copyJointGroupPositions(joint_model_group, joint_group_positions);
 
-  // Now, let's modify one of the joints, plan to the new joint space goal, and visualize the plan.
-  joint_group_positions[0] = 0.2;
+  // Now, let's modify the joints to stow the arm, plan to the new joint space goal, and visualize the plan.
+  joint_group_positions[3] = 0.2;
+  joint_group_positions[4] = 0;
+  joint_group_positions[5] = 0;
+  joint_group_positions[6] = 0;
+  joint_group_positions[7] = 0;
+  joint_group_positions[8] = 229;
   move_group.setJointValueTarget(joint_group_positions);
 
-  // We lower the allowed maximum velocity and acceleration to 50% of their maximum.
+  // We lower the allowed maximum velocity and acceleration to 80% of their maximum.
   // The default values are 10% (0.1).
   // Set your preferred defaults in the joint_limits.yaml file of your robot's moveit_config
   // or set explicit factors in your code if you need your robot to move faster.
-  move_group.setMaxVelocityScalingFactor(0.5);
-  move_group.setMaxAccelerationScalingFactor(0.5);
+  move_group.setMaxVelocityScalingFactor(0.8);
+  move_group.setMaxAccelerationScalingFactor(0.8);
 
-  success = (move_group.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
-  move_group.move();
-  RCLCPP_INFO(LOGGER, "Visualizing plan 2 (joint space goal) %s", success ? "" : "FAILED");
+  bool success = (move_group.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+  RCLCPP_INFO(LOGGER, "Visualizing plan (joint space goal) %s", success ? "" : "FAILED");
 
   // Visualize the plan in RViz:
   visual_tools.deleteAllMarkers();
   visual_tools.publishText(text_pose, "Joint_Space_Goal", rvt::WHITE, rvt::XLARGE);
-  visual_tools.publishTrajectoryLine(my_plan.trajectory_, joint_model_group);
+  visual_tools.publishTrajectoryLine(my_plan.trajectory_, ee_parent_link, joint_model_group);
   visual_tools.trigger();
   visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to continue the demo");
 
+  move_group.move();
   // END_TUTORIAL
   visual_tools.deleteAllMarkers();
   visual_tools.trigger();
