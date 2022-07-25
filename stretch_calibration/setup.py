ad3cde9f0f9dc194f31899023da324d841aa9185
hello-chintan <cdesai@hello-robot.com>
2022-07-25 14:04:30 -0400

    Update package manifest dependencies

---
+++ stretch_calibration/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+from glob import glob
+
+package_name = 'stretch_calibration'
+
+setup(
+    name=package_name,
+    version='0.2.0',
+    packages=find_packages(),
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
+        ('share/' + package_name + '/config', glob('config/*')),
+    ],
+    install_requires=['setuptools'],
+    url='https://github.com/hello-robot/stretch_ros2',
+    license='Apache License 2.0',
+    author='Hello Robot Inc.',
+    author_email='support@hello-robot.com',
+    description='The stretch calibration package',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            # 'collect_head_calibration_data = stretch_calibration.collect_head_calibration_data:main',
+            # 'process_head_calibration_data = stretch_calibration.process_head_calibration_data:main',
+        ],
+    },
+)
