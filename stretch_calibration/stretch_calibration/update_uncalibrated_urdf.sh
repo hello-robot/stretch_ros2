27b0e5c00fa3c4d483903af42ebe72fcb5e9e2f1
hello-chintan <cdesai@hello-robot.com>
2022-08-25 13:28:46 -0400

    Remove old and redundant files

--- stretch_calibration/stretch_calibration/update_uncalibrated_urdf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo ""
-echo "Convert the current xacro files to a fresh uncalibrated URDF file."
-echo ""
-
-echo "ros2 run xacro xacro `ros2 pkg prefix --share stretch_description`/urdf/stretch_description.xacro > `ros2 pkg prefix --share stretch_description`/urdf/stretch_uncalibrated.urdf"
-
-ros2 run xacro xacro `ros2 pkg prefix --share stretch_description`/urdf/stretch_description.xacro > `ros2 pkg prefix --share stretch_description`/urdf/stretch_uncalibrated.urdf
