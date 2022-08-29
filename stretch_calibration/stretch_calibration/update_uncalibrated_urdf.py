447346ddccfae4b5f37884aac00f0e5168a99b1b
hello-chintan <cdesai@hello-robot.com>
2022-08-29 10:40:23 -0400

    Exit with error codes and update files in src dir

--- stretch_calibration/stretch_calibration/update_uncalibrated_urdf.py
+++ stretch_calibration/stretch_calibration/update_uncalibrated_urdf.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import subprocess
 import shlex
+import sys
+
 
 def main():
     print("---")
@@ -8,20 +10,30 @@ def main():
     print("---")
 
     print("ros2 run xacro xacro `ros2 pkg prefix --share stretch_description`/urdf/stretch_description.xacro > `ros2 pkg prefix --share stretch_description`/urdf/stretch_uncalibrated.urdf")
-    
-    bashCommand = "ros2 pkg prefix --share stretch_description"
+
+    bashCommand = "ros2 pkg prefix stretch_description"
     process = subprocess.run(shlex.split(bashCommand), capture_output=True, text=True)  
     filepath = process.stdout
     filepath = filepath[0 : len(process.stdout)-1]
+    newpath = "/src/stretch_ros2/stretch_description"
+    minuspath = "/install/stretch_description"
+    minuslength = len(minuspath) + 1
+    filepath = filepath[0 : len(process.stdout) - minuslength]
+    newpath = "{0}{1}".format(filepath, newpath)
+    print(newpath)
 
-    bashCommand = "ros2 run xacro xacro {}/urdf/stretch_description.xacro".format(filepath)
+    bashCommand = "ros2 run xacro xacro {}/urdf/stretch_description.xacro".format(newpath)
     process = subprocess.run(shlex.split(bashCommand), capture_output=True, text=True)
+    if(process.stderr != ''):
+        print("There was an error while running the script: {}".format(process.stderr), file=sys.stderr)
+        sys.exit(1)
     urdf = process.stdout
 
-    test_filepath = "{}/urdf/stretch_uncalibrated.urdf".format(filepath)
+    test_filepath = "{}/urdf/stretch_uncalibrated.urdf".format(newpath)
     with open(test_filepath, "w") as open_file:
         print(urdf, file=open_file)
         open_file.close()
+
 
 if __name__ == '__main__':
      main()
