fedb03e42d7098aa619314c126ea91689708f41a
hello-chintan <cdesai@hello-robot.com>
2022-10-01 23:03:12 -0400

    MoveIt draw initial commit

---
+++ stretch_moveit_config/launch/moveit_draw.launch.py
@@ -0,0 +1,92 @@
+from ament_index_python.packages import get_package_share_path
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command
+from launch.substitutions import LaunchConfiguration
+
+from launch_ros.actions import Node
+
+# from hybrid_planning_common import (generate_common_hybrid_launch_description, load_yaml)
+
+
+def generate_launch_description():
+    moveit_config_path = get_package_share_path('stretch_moveit_config')
+    stretch_core_path = get_package_share_path('stretch_core')
+
+    ld = LaunchDescription()
+    ld.add_action(DeclareLaunchArgument('pipeline', default_value='ompl', description='specify the planning pipeline'))
+    ld.add_action(DeclareLaunchArgument('db', default_value='false', choices=['true', 'false'],
+                                        description='By default, we do not start a database (it can be large)'))
+
+    ld.add_action(DeclareLaunchArgument('db_path', default_value=str(moveit_config_path / 'default_warehouse_mongo_db'),
+                                        description='Allow user to specify database location'))
+    ld.add_action(DeclareLaunchArgument('debug', default_value='false', choices=['true', 'false'],
+                                        description='By default, we are not in debug mode'))
+
+    ld.add_action(DeclareLaunchArgument('use_stretch_driver', default_value='true', choices=['true', 'false'],
+                                        description='Allow user to launch Stretch Driver separately'))
+    ld.add_action(DeclareLaunchArgument('use_rviz', default_value='true', choices=['true', 'false']))
+
+    # Load the URDF, SRDF and other .yaml configuration files
+    robot_description_content = Command(['xacro ',
+                                         str(get_package_share_path('stretch_description') / 'urdf' / 'stretch.urdf')])
+    with open(moveit_config_path / 'config/stretch_description.srdf', 'r') as f:
+        semantic_content = f.read()
+
+    stretch_driver_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([str(stretch_core_path), '/launch/stretch_driver.launch.py']),
+        launch_arguments={'mode': 'manipulation', 'broadcast_odom_tf': 'True'}.items(),
+        condition=IfCondition(LaunchConfiguration('use_stretch_driver'))
+    )
+    ld.add_action(stretch_driver_launch)
+
+    # Run the main MoveIt executable
+    move_group_launch_py = PythonLaunchDescriptionSource(str(moveit_config_path / 'launch/move_group.launch.py'))
+    move_group_launch_args = {
+        'allow_trajectory_execution': 'true',
+        'fake_execution': 'false',
+        'info': 'true',
+        'debug': LaunchConfiguration('debug'),
+        'pipeline': LaunchConfiguration('pipeline'),
+        'robot_description': robot_description_content,
+        'semantic_config': semantic_content,
+    }
+    move_group_launch = IncludeLaunchDescription(move_group_launch_py,
+                                                 launch_arguments=move_group_launch_args.items())
+    ld.add_action(move_group_launch)
+
+    moveit_draw_py = PythonLaunchDescriptionSource(str(moveit_config_path / 'launch/movegroup_test_draw.launch.py'))
+    moveit_draw = IncludeLaunchDescription(moveit_draw_py,
+                                                 launch_arguments=move_group_launch_args.items())
+    ld.add_action(moveit_draw)
+
+    # Run Rviz and load the default config to see the state of the move_group node
+    moveit_rviz_launch_py = PythonLaunchDescriptionSource(
+        str(moveit_config_path / 'launch/moveit_rviz.launch.py')
+    )
+    moveit_rviz_args = {
+        'rviz_config': str(moveit_config_path / 'launch/moveit.rviz'),
+        'debug': LaunchConfiguration('debug'),
+        'robot_description': robot_description_content,
+        'semantic_config': semantic_content,
+    }
+    moveit_rviz_launch = IncludeLaunchDescription(moveit_rviz_launch_py, launch_arguments=moveit_rviz_args.items(),
+                                                  condition=IfCondition(LaunchConfiguration('use_rviz')))
+    ld.add_action(moveit_rviz_launch)
+
+    # If database loading was enabled, start mongodb as well
+    warehouse_launch_py = PythonLaunchDescriptionSource(
+        str(moveit_config_path / 'launch/warehouse_db.launch.py')
+    )
+    warehouse_launch = IncludeLaunchDescription(warehouse_launch_py,
+                                                launch_arguments={'moveit_warehouse_database_path':
+                                                                  LaunchConfiguration('db_path')}.items(),
+                                                condition=IfCondition(LaunchConfiguration('db'))
+                                                )
+    ld.add_action(warehouse_launch)
+
+    return ld
---
+++ stretch_moveit_config/src/moveit_draw.cpp
@@ -0,0 +1,106 @@
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit/planning_scene_monitor/planning_scene_monitor.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+#include <moveit_msgs/msg/display_robot_state.hpp>
+#include <moveit_msgs/msg/display_trajectory.hpp>
+#include <moveit_msgs/msg/attached_collision_object.hpp>
+#include <moveit_msgs/msg/collision_object.hpp>
+#include <moveit_visual_tools/moveit_visual_tools.h>
+
+static const rclcpp::Logger LOGGER = rclcpp::get_logger("moveit_draw_demo");
+
+int main(int argc, char** argv)
+{
+  rclcpp::init(argc, argv);
+  rclcpp::NodeOptions node_options;
+  node_options.automatically_declare_parameters_from_overrides(true);
+  auto moveit_draw_node = rclcpp::Node::make_shared("moveit_draw", node_options);
+
+  rclcpp::executors::SingleThreadedExecutor executor;
+  executor.add_node(moveit_draw_node);
+  std::thread([&executor]() { executor.spin(); }).detach();
+
+  // ########################### Setup ##########################
+  static const std::string PLANNING_GROUP_ARM = "stretch_arm";
+
+  moveit::planning_interface::MoveGroupInterface move_group_arm(moveit_draw_node, PLANNING_GROUP_ARM);
+
+  move_group_arm.setMaxVelocityScalingFactor(1.0);
+  move_group_arm.setMaxAccelerationScalingFactor(1.0);
+
+  const moveit::core::JointModelGroup* joint_model_group;
+  const moveit::core::LinkModel* ee_parent_link = 
+      move_group_arm.getCurrentState()->getLinkModel("link_grasp_center"); // the link name can be changed here to visualize the trajectory of the corresponding link
+
+  namespace rvt = rviz_visual_tools;
+  moveit_visual_tools::MoveItVisualTools visual_tools(moveit_draw_node, "odom", "move_group_visualization",
+                                                      move_group_arm.getRobotModel());
+  visual_tools.deleteAllMarkers();
+  visual_tools.loadRemoteControl();
+  visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to start the demo");
+  visual_tools.trigger();
+
+  Eigen::Isometry3d text_pose = Eigen::Isometry3d::Identity();
+  text_pose.translation().z() = 1.0;
+  visual_tools.publishText(text_pose, "MoveGroupInterface_Demo", rvt::WHITE, rvt::XLARGE);
+  visual_tools.trigger();
+
+  RCLCPP_INFO(LOGGER, "Planning frame: %s", move_group_arm.getPlanningFrame().c_str());
+  RCLCPP_INFO(LOGGER, "End effector link: %s", move_group_arm.getEndEffectorLink().c_str());
+  RCLCPP_INFO(LOGGER, "Available Planning Groups:");
+  std::copy(move_group_arm.getJointModelGroupNames().begin(), move_group_arm.getJointModelGroupNames().end(),
+            std::ostream_iterator<std::string>(std::cout, ", "));
+
+  moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+
+  bool success;
+  std::vector<double> joint_group_positions;
+  moveit::core::RobotStatePtr current_state;
+
+  // ########################### Pose Goal ##########################
+
+  joint_model_group = move_group_arm.getCurrentState()->getJointModelGroup(PLANNING_GROUP_ARM);
+  ee_parent_link = move_group_arm.getCurrentState()->getLinkModel("link_grasp_center");
+
+  visual_tools.deleteAllMarkers();
+  visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to start the demo");
+  visual_tools.trigger();
+
+  text_pose = Eigen::Isometry3d::Identity();
+  text_pose.translation().z() = 1.0;
+  visual_tools.publishText(text_pose, "Pose Goal", rvt::WHITE, rvt::XLARGE);
+  visual_tools.trigger();
+
+  geometry_msgs::msg::PoseStamped currentPose;
+  currentPose = move_group_arm.getCurrentPose();
+
+  move_group_arm.setStartStateToCurrentState();
+
+  geometry_msgs::msg::Pose target_pose1;
+  target_pose1.orientation.x = currentPose.pose.orientation.x;
+  target_pose1.orientation.y = currentPose.pose.orientation.y;
+  target_pose1.orientation.z = currentPose.pose.orientation.z;
+  target_pose1.orientation.w = currentPose.pose.orientation.w;
+  target_pose1.position.x = currentPose.pose.position.x;
+  target_pose1.position.y = currentPose.pose.position.y;
+  target_pose1.position.z = 1.25;
+
+  move_group_arm.setApproximateJointValueTarget(target_pose1, "link_wrist_yaw");
+
+  success = (move_group_arm.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+
+  RCLCPP_INFO(LOGGER, "Visualizing plan 1 (pose goal) %s", success ? "" : "FAILED");
+
+  visual_tools.deleteAllMarkers();
+  visual_tools.publishTrajectoryLine(my_plan.trajectory_, ee_parent_link, joint_model_group);
+  visual_tools.trigger();
+  visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to continue the demo");
+  
+  move_group_arm.move();
+
+  visual_tools.deleteAllMarkers();
+  visual_tools.trigger();
+
+  rclcpp::shutdown();
+  return 0;
+}
