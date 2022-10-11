c8a44717e37ee514087f468e8b1a6583df0dd3d2
hello-chintan <cdesai@hello-robot.com>
2022-10-11 12:40:32 -0400

    Add objects and faces detection launch  files

--- stretch_deep_perception/setup.py
+++ stretch_deep_perception/setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+from glob import glob
 
 package_name = 'stretch_deep_perception'
 
@@ -10,6 +11,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
+        ('share/' + package_name + '/config', glob('config/*')),
+        ('share/' + package_name + '/rviz', glob('rviz/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
