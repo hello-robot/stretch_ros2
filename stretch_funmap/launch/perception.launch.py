0842bf4d955bad1fa9cd8ea18171a8168ac8a309
Chintan Desai <cdesai@hello-robot.com>
2023-05-23 13:28:29 -0700

    Add perception launch

---
+++ stretch_funmap/launch/perception.launch.py
@@ -0,0 +1,15 @@
+import os
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration, Command
+from launch.launch_description_sources import PythonLaunchDescriptionSource, FrontendLaunchDescriptionSource
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+### Add launch files and nodes specific to perception here
+def generate_launch_description():
+    return LaunchDescription([
+
+    ])
