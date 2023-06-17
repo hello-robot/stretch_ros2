158735a5ffaac28a8507ee0ea779b906e141ae5b
Atharva Pusalkar <atharva@hello-robot.com>
2023-06-16 18:29:19 -0700

    Add keyboard teleop services to test funmap

--- stretch_core/stretch_core/keyboard_teleop.py
+++ stretch_core/stretch_core/keyboard_teleop.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
 
+import argparse as ap
+from functools import partial
 import math
+import sys
+
 from .keyboard import KBHit
 
 import rclpy
+from rclpy.action import ActionClient
 from rclpy.duration import Duration
+from rclpy.node import Node
+from rclpy.parameter import Parameter
 from std_msgs.msg import String
+from std_srvs.srv import Trigger
 from sensor_msgs.msg import JointState
 from control_msgs.action import FollowJointTrajectory
 from trajectory_msgs.msg import JointTrajectoryPoint, MultiDOFJointTrajectoryPoint
@@ -16,8 +24,15 @@ import hello_helpers.hello_misc as hm
 
 class GetKeyboardCommands:
 
-    def __init__(self):
+    def __init__(self, node, mapping_on=False, hello_world_on=False, open_drawer_on=False, clean_surface_on=False, grasp_object_on=False, handover_object_on=False):
         self.kb = KBHit()
+        self.mapping_on = mapping_on
+        self.hello_world_on = hello_world_on
+        self.open_drawer_on = open_drawer_on
+        self.clean_surface_on = clean_surface_on
+        self.grasp_object_on = grasp_object_on
+        self.handover_object_on = handover_object_on
+        self.node = node
         self.step_size = 'medium'
         self.rad_per_deg = math.pi/180.0
         self.small_deg = 3.0
@@ -80,7 +95,58 @@ class GetKeyboardCommands:
 
         if self.kb.kbhit(): # Returns True if any key pressed
             c = self.kb.getch()
-            
+
+        ####################################################
+        ## MOSTLY MAPPING RELATED CAPABILITIES
+        ## (There are non-mapping outliers.)
+        ####################################################
+        # Sequential performs a fixed number of autonomus mapping iterations
+        if (c == '!') and self.mapping_on:
+            number_iterations = 4
+            for n in range(number_iterations):
+                # Trigger a 3D scan with the D435i
+                trigger_request = Trigger.Request() 
+                trigger_result = node.trigger_head_scan_service.call_async(trigger_request)
+
+                # Trigger driving the robot to the estimated next best place to scan
+                trigger_request = Trigger.Request() 
+                trigger_result = node.trigger_drive_to_scan_service.call_async(trigger_request)
+                
+        # Trigger localizing the robot to a new pose anywhere on the current map
+        if ((c == '+') or (c == '=')) and self.mapping_on:
+            trigger_request = Trigger.Request() 
+            trigger_result = node.trigger_global_localization_service.call_async(trigger_request)
+
+        # Trigger localizing the robot to a new pose that is near its current pose on the map
+        if ((c == '-') or (c == '_')) and self.mapping_on:
+            trigger_request = Trigger.Request() 
+            trigger_result = node.trigger_local_localization_service.call_async(trigger_request)
+
+        # Trigger driving the robot to the estimated next best place to perform a 3D scan
+        if ((c == '\\') or (c == '|')) and self.mapping_on:
+            trigger_request = Trigger.Request() 
+            trigger_result = node.trigger_drive_to_scan_service.call_async(trigger_request)
+
+        # Trigger performing a 3D scan using the D435i
+        if (c == ' ') and self.mapping_on:
+            trigger_request = Trigger.Request() 
+            trigger_result = node.trigger_head_scan_service.call_async(trigger_request)
+
+        # Trigger rotating the mobile base to align with the nearest 3D cliff detected visually
+        if ((c == '[') or (c == '{')) and self.mapping_on:
+            trigger_request = Trigger.Request() 
+            trigger_result = node.trigger_align_with_nearest_cliff_service.call_async(trigger_request)
+
+        # DEPRECATED: Trigger extend arm until contact
+        if ((c == ']') or (c == '}')) and self.mapping_on:
+            trigger_request = Trigger.Request() 
+            trigger_result = node.trigger_reach_until_contact_service.call_async(trigger_request)
+
+        # DEPRECATED: Trigger lower arm until contact
+        if ((c == ':') or (c == ';')) and self.mapping_on:
+            trigger_request = Trigger.Request() 
+            trigger_result = node.trigger_lower_until_contact_service.call_async(trigger_request)
+
         ####################################################
         ## BASIC KEYBOARD TELEOPERATION COMMANDS
         ####################################################
@@ -154,13 +220,37 @@ class GetKeyboardCommands:
         return command
 
 
-class KeyboardTeleopNode(hm.HelloNode):
+class KeyboardTeleopNode(Node):
 
     def __init__(self):
-        hm.HelloNode.__init__(self)
-        self.keys = GetKeyboardCommands()
+        super().__init__('keyboard_teleop')
+        self.declare_parameter('mapping_on', False)
+        self.declare_parameter('hello_world_on', False)
+        self.declare_parameter('open_drawer_on', False)
+        self.declare_parameter('clean_surface_on', False)
+        self.declare_parameter('grasp_object_on', False)
+        self.declare_parameter('handover_object_on', False)
+
+        self.mapping_on = self.get_parameter('mapping_on').value
+        self.hello_world_on = self.get_parameter('hello_world_on').value
+        self.open_drawer_on = self.get_parameter('open_drawer_on').value
+        self.clean_surface_on = self.get_parameter('clean_surface_on').value
+        self.grasp_object_on = self.get_parameter('grasp_object_on').value
+        self.handover_object_on = self.get_parameter('handover_object_on').value
+
+        self.keys = GetKeyboardCommands(self,
+                                        self.mapping_on,
+                                        self.hello_world_on,
+                                        self.open_drawer_on,
+                                        self.clean_surface_on,
+                                        self.grasp_object_on,
+                                        self.handover_object_on)
+
         self.joint_state = JointState()
         self.robot_mode = String()
+
+        if self.clean_surface_on:
+            self.get_logger().info("Clean surface demo enabled.")
         
     def joint_states_callback(self, joint_state):
         self.joint_state = joint_state
@@ -248,14 +338,48 @@ class KeyboardTeleopNode(hm.HelloNode):
 
 
     def main(self):
-        rclpy.init()
-        hm.HelloNode.main(self, 'keyboard_teleop', 'keyboard_teleop', wait_for_first_pointcloud=False)
-
         self.joint_states_sub = self.create_subscription(JointState, '/stretch/joint_states', self.joint_states_callback, 1)
         self.joint_states_sub
 
         self.robot_mode_sub = self.create_subscription(String, 'mode', self.mode_callback, 10)
         self.robot_mode_sub
+
+        self.trajectory_client = ActionClient(self, FollowJointTrajectory, '/stretch_controller/follow_joint_trajectory')
+        server_reached = self.trajectory_client.wait_for_server(timeout_sec=60.0)
+        if not server_reached:
+            self.get_logger().error('Unable to connect to arm action server. Timeout exceeded.')
+            sys.exit()
+
+        if self.mapping_on: 
+            self.get_logger().info('Node ' + self.get_name() + ' waiting to connect to /funmap/trigger_head_scan.')
+
+            self.trigger_head_scan_service = self.create_client(Trigger, '/funmap/trigger_head_scan')
+            self.trigger_head_scan_service.wait_for_service()
+            self.get_logger().info('Node ' + self.get_name() + ' connected to /funmap/trigger_head_scan.')
+
+            self.trigger_drive_to_scan_service = self.create_client(Trigger, '/funmap/trigger_drive_to_scan')
+            self.trigger_drive_to_scan_service.wait_for_service()
+            self.get_logger().info('Node ' + self.get_name() + ' connected to /funmap/trigger_drive_to_scan.')
+
+            self.trigger_global_localization_service = self.create_client(Trigger, '/funmap/trigger_global_localization')
+            self.trigger_global_localization_service.wait_for_service()
+            self.get_logger().info('Node ' + self.get_name() + ' connected to /funmap/trigger_global_localization.')
+
+            self.trigger_local_localization_service = self.create_client(Trigger, '/funmap/trigger_local_localization')
+            self.trigger_local_localization_service.wait_for_service()
+            self.get_logger().info('Node ' + self.get_name() + ' connected to /funmap/trigger_local_localization.')
+
+            self.trigger_align_with_nearest_cliff_service = self.create_client(Trigger, '/funmap/trigger_align_with_nearest_cliff')
+            self.trigger_align_with_nearest_cliff_service.wait_for_service()
+            self.get_logger().info('Node ' + self.get_name() + ' connected to /funmap/trigger_align_with_nearest_cliff.')
+
+            self.trigger_reach_until_contact_service = self.create_client(Trigger, '/funmap/trigger_reach_until_contact')
+            self.trigger_reach_until_contact_service.wait_for_service()
+            self.get_logger().info('Node ' + self.get_name() + ' connected to /funmap/trigger_reach_until_contact.')
+
+            self.trigger_lower_until_contact_service = self.create_client(Trigger, '/funmap/trigger_lower_until_contact')
+            self.trigger_lower_until_contact_service.wait_for_service()
+            self.get_logger().info('Node ' + self.get_name() + ' connected to /funmap/trigger_lower_until_contact.')
         
         self.keys.print_commands()
         while rclpy.ok():
@@ -269,6 +393,8 @@ class KeyboardTeleopNode(hm.HelloNode):
 
 def main():
     try:
+        rclpy.init()
+
         node = KeyboardTeleopNode()
         node.main()
     except KeyboardInterrupt:
