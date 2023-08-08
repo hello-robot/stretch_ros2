f9c5a3e07933fbfe2ad7c5ea30776efefda02250
hello-chintan <cdesai@hello-robot.com>
2023-08-07 17:35:52 -0700

    Update hello world with HelloNode

--- stretch_demos/stretch_demos/hello_world.py
+++ stretch_demos/stretch_demos/hello_world.py
@@ -1,121 +1,39 @@
 #!/usr/bin/env python3
 
-from sensor_msgs.msg import JointState
-from geometry_msgs.msg import Twist
-from nav_msgs.msg import Odometry
-
 import rclpy
-from rclpy.action import ActionClient, ActionServer
 from rclpy.callback_groups import ReentrantCallbackGroup
-from rclpy.duration import Duration
-from rclpy.executors import MultiThreadedExecutor
 import rclpy.logging
-from rclpy.node import Node
-from rclpy.time import Time
 
-from control_msgs.action import FollowJointTrajectory
-from trajectory_msgs.msg import JointTrajectoryPoint
-from sensor_msgs.msg import PointCloud2
+from sensor_msgs.msg import JointState
 from std_srvs.srv import Trigger
 
-import math
 import time
 import threading
-import sys
-import tf2_ros
-import argparse as ap        
 
 import hello_helpers.hello_misc as hm
 import stretch_funmap.navigate as nv
 
 
-class HelloWorldNode(Node):
+class HelloWorldNode(hm.HelloNode):
 
     def __init__(self):
-        super().__init__('hello_world')
+        hm.HelloNode.__init__(self)
         self.rate = 10.0
         self.joint_states = None
         self.joint_states_lock = threading.Lock()
         self.move_base = nv.MoveBase(self)
         self.letter_height_m = 0.2
         self.letter_top_lift_m = 1.08
-        self.logger = self.get_logger()
-        self.tf2_buffer = tf2_ros.Buffer()
-        self.tf_listener = tf2_ros.transform_listener.TransformListener(self.tf2_buffer, self)
-        self.move_to_pose_complete = False
-        self.unsuccessful_status = [-100, 100, FollowJointTrajectory.Result.PATH_TOLERANCE_VIOLATED, FollowJointTrajectory.Result.INVALID_JOINTS, FollowJointTrajectory.Result.INVALID_GOAL, FollowJointTrajectory.Result.GOAL_TOLERANCE_VIOLATED, FollowJointTrajectory.Result.OLD_HEADER_TIMESTAMP]
-        self._get_result_future = None
         
     def joint_states_callback(self, joint_states):
         with self.joint_states_lock: 
             self.joint_states = joint_states
-
-    def goal_response(self, future: rclpy.task.Future):
-        if not future or not future.result():
-            return False
-        goal_handle = future.result()
-        if not goal_handle.accepted:
-            self.move_to_pose_complete = True
-            return
-
-        self._get_result_future = goal_handle.get_result_async()
-
-    def get_result(self, future: rclpy.task.Future):
-        if not future or not future.result():
-            return
-
-        result = future.result().result
-        error_code = result.error_code
-        self.move_to_pose_complete = True
-    
-    def move_to_pose(self, pose, return_before_done=False, custom_contact_thresholds=False):
-        self.move_to_pose_complete = False
-        joint_names = [key for key in pose]
-        point = JointTrajectoryPoint()
-        point.time_from_start = Duration(seconds=0.0).to_msg()
-
-        trajectory_goal = FollowJointTrajectory.Goal()
-        trajectory_goal.goal_time_tolerance = Duration(seconds=1.0).to_msg()
-        trajectory_goal.trajectory.joint_names = joint_names
-        if not custom_contact_thresholds: 
-            joint_positions = [pose[key] for key in joint_names]
-            point.positions = joint_positions
-            trajectory_goal.trajectory.points = [point]
-        else:
-            pose_correct = all([len(pose[key])==2 for key in joint_names])
-            if not pose_correct:
-                self.logger.error("HelloNode.move_to_pose: Not sending trajectory due to improper pose. custom_contact_thresholds requires 2 values (pose_target, contact_threshold_effort) for each joint name, but pose = {0}".format(pose))
-                return
-            joint_positions = [pose[key][0] for key in joint_names]
-            joint_efforts = [pose[key][1] for key in joint_names]
-            point.positions = joint_positions
-            point.effort = joint_efforts
-            trajectory_goal.trajectory.points = [point]
-        trajectory_goal.trajectory.header.stamp = self.get_clock().now().to_msg()
-        self._send_goal_future = self.trajectory_client.send_goal_async(trajectory_goal)
-
-        if not return_before_done:
-            time_start = time.time()
-            self._get_result_future = None
-
-            while self._get_result_future == None and (time.time() - time_start) < 10:
-                self.goal_response(self._send_goal_future)
-
-            if self._get_result_future == None:
-                return self._send_goal_future
-
-            time_start = time.time()
-            while not self.move_to_pose_complete and (time.time() - time_start) < 10:
-                self.get_result(self._get_result_future)
-        
-        return self._send_goal_future
 
     def align_to_surface(self):
         self.logger.info('align_to_surface')
         trigger_request = Trigger.Request() 
         trigger_result = self.trigger_align_with_nearest_cliff_service.call_async(trigger_request)
         self.logger.info('trigger_result = {0}'.format(trigger_result))
-
 
     def backoff_after_contact(self):
         self.logger.info('backoff after contact')
@@ -157,7 +75,6 @@ class HelloWorldNode(Node):
         self.move_to_pose(pose, custom_contact_thresholds=True)
         time.sleep(1.0) # Give it time to backoff before the next move. Otherwise it may not backoff.
 
-        
     def vertical_line(self, move_down, half=False):
         if move_down: 
             self.logger.info('vertical_line moving down')
@@ -204,7 +121,6 @@ class HelloWorldNode(Node):
         self.align_to_surface()
         self.horizontal_line(move_right=True, half=True)
 
-
     def move_arm_out_of_the_way(self):
         initial_pose = {'wrist_extension': 0.01,
                         'joint_lift': 0.8,
@@ -212,7 +128,6 @@ class HelloWorldNode(Node):
 
         self.logger.info('Move arm out of the way of the camera.')
         self.move_to_pose(initial_pose)
-
         
     def move_to_initial_configuration(self):
         initial_pose = {'wrist_extension': 0.01,
@@ -313,8 +228,10 @@ class HelloWorldNode(Node):
             message='Completed writing hello!'
             )
 
-    
     def main(self):
+        hm.HelloNode.main(self, 'hello_world', 'hello_world', wait_for_first_pointcloud=False)
+
+        self.logger = self.get_logger()
         self.callback_group = ReentrantCallbackGroup()
 
         self.joint_states_subscriber = self.create_subscription(JointState, '/stretch/joint_states', callback=self.joint_states_callback, qos_profile=1, callback_group=self.callback_group)
@@ -330,20 +247,12 @@ class HelloWorldNode(Node):
         self.trigger_reach_until_contact_service.wait_for_service()
         self.logger.info('Node ' + self.get_name() + ' connected to /funmap/trigger_reach_until_contact.')
 
-        self.trajectory_client = ActionClient(self, FollowJointTrajectory, '/stretch_controller/follow_joint_trajectory', callback_group=self.callback_group)
-        server_reached = self.trajectory_client.wait_for_server(timeout_sec=60.0)
-        if not server_reached:
-            self.get_logger().error('Unable to connect to joint_trajectory_server. Timeout exceeded.')
-            sys.exit()
-
 def main():
     try:
-        rclpy.init()
         node = HelloWorldNode()
         node.main()
-        executor = MultiThreadedExecutor(num_threads=6)
-        executor.add_node(node=node)
-        executor.spin()
+
+        node.new_thread.join()
     except KeyboardInterrupt:
         rclpy.logging.get_logger('hello_world').info('interrupt received, so shutting down')
 
