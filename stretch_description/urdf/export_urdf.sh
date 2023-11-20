99cda32feec0cf67e9b13cc862b4f293dd678c1e
Binit Shah <bshah@hello-robot.com>
2023-11-19 21:46:39 -0500

    Update export_urdf.sh

--- stretch_description/urdf/export_urdf.sh
+++ stretch_description/urdf/export_urdf.sh
@@ -5,15 +5,15 @@ echo "Prior to running this script make sure you have used stretch_calibration t
 echo ""
 
 # Move previous exported_urdf to exported_urdf_previous.
-echo "Move previous exported_urdf to exported_urdf_previous."
-echo "mv ./exported_urdf ./exported_urdf_previous"
-mv ./exported_urdf ./exported_urdf_previous
-echo ""
+if [[ -d ./exported_urdf ]]; then
+    echo "Move previous exported_urdf to exported_urdf_previous."
+    echo "mv ./exported_urdf ./exported_urdf_previous"
+    mv ./exported_urdf ./exported_urdf_previous
+    echo ""
+fi
 
 # Create new exported URDF directories.
 echo "Create new exported URDF directories."
-echo "mkdir -p ./exported_urdf/" 
-mkdir -p ./exported_urdf/
 echo "mkdir -p ./exported_urdf/meshes/"
 mkdir -p ./exported_urdf/meshes/
 echo ""
@@ -30,16 +30,18 @@ echo ""
 echo "Replace the mesh file locations in the exported URDF with local directories."
 OLD_NAME="package://stretch_description/"
 NEW_NAME="./"
-echo "rpl -i $OLD_NAME $NEW_NAME ./exported_urdf/stretch.urdf"
-rpl -i $OLD_NAME $NEW_NAME ./exported_urdf/stretch.urdf
+echo "rpl -q --encoding UTF-8 -i $OLD_NAME $NEW_NAME ./exported_urdf/stretch.urdf"
+rpl -q --encoding UTF-8 -i $OLD_NAME $NEW_NAME ./exported_urdf/stretch.urdf
 echo ""
 
 # Copy D435i mesh from the realsense2_description ROS package to the exported URDF.
 echo "Copy D435i mesh from the realsense2_description ROS package to the exported URDF."
 echo "cp `ros2 pkg prefix realsense2_description`/share/realsense2_description/meshes/d435.dae ./exported_urdf/meshes/"
 cp `ros2 pkg prefix realsense2_description`/share/realsense2_description/meshes/d435.dae ./exported_urdf/meshes/
-echo "rpl -i "package://realsense2_description/" "./" ./exported_urdf/stretch.urdf"
-rpl -i "package://realsense2_description/" "./" ./exported_urdf/stretch.urdf
+OLD_NAME="file:///home/$USER/ament_ws/install/realsense2_description/share/realsense2_description/"
+NEW_NAME="./"
+echo "rpl -q --encoding UTF-8 -i $OLD_NAME $NEW_NAME ./exported_urdf/stretch.urdf"
+rpl -q --encoding UTF-8 -i $OLD_NAME $NEW_NAME ./exported_urdf/stretch.urdf
 echo ""
 
 # copy controller calibration file used by stretch ROS
@@ -55,5 +57,6 @@ cp export_urdf_license_template.md  ./exported_urdf/LICENSE.md
 
 echo "Copy the exported URDF to the standard Stretch directory."
 echo "cp -rf exported_urdf/* $HELLO_FLEET_PATH/$HELLO_FLEET_ID/exported_urdf"
+mkdir -p $HELLO_FLEET_PATH/$HELLO_FLEET_ID/exported_urdf
 cp -rf exported_urdf/* $HELLO_FLEET_PATH/$HELLO_FLEET_ID/exported_urdf
 echo ""
