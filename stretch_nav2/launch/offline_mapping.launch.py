945691ea449fe6836ae35f467aa295e5984294f4
Mohamed Fazil <mfazil@hello-robot.com>
2024-09-09 10:01:14 -0700

    remove joystick teleop twist and instead use gamepad mode

--- stretch_nav2/launch/offline_mapping.launch.py
+++ stretch_nav2/launch/offline_mapping.launch.py
@@ -25,14 +25,13 @@ def generate_launch_description():
 
     stretch_driver_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([stretch_core_path, '/launch/stretch_driver.launch.py']),
-        launch_arguments={'mode': 'navigation', 'broadcast_odom_tf': 'True'}.items())
+        launch_arguments={'mode': 'gamepad', 'broadcast_odom_tf': 'True'}.items())
 
     rplidar_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([stretch_core_path, '/launch/rplidar.launch.py']))
 
-    base_teleop_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([stretch_navigation_path, '/launch/teleop_twist.launch.py']),
-        launch_arguments={'teleop_type': LaunchConfiguration('teleop_type')}.items())
+    offline_mapping_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([get_package_share_directory('slam_toolbox'), '/launch/offline_launch.py']))
 
     rviz_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([get_package_share_directory('nav2_bringup'), '/launch/rviz_launch.py']),
@@ -44,7 +43,7 @@ def generate_launch_description():
         declare_use_sim_time_argument,
         stretch_driver_launch,
         rplidar_launch,
-        base_teleop_launch,
+        offline_mapping_launch,
         rviz_launch,
     ])
 
