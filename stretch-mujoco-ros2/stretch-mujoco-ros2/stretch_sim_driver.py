21f6870299741b9495e98c10a05358a383060aea
Mo Xu <xmo@cs.wisc.edu>
2025-02-19 02:41:57 -0600

    update ros wrapper

--- stretch-mujoco-ros2/stretch-mujoco-ros2/stretch_sim_driver.py
+++ stretch-mujoco-ros2/stretch-mujoco-ros2/stretch_sim_driver.py
@@ -5,6 +5,7 @@ import copy
 import yaml
 import numpy as np
 import threading
+from .rwlock import RWLock
 
 import tf2_ros
 from tf_transformations import quaternion_from_euler
@@ -44,8 +45,47 @@ class StretchSimDriver(Node):
         self.robot_sim = StretchMujocoSimulator(scene_xml_path)
         self.robot_sim.start()
         
+        self.robot_mode_rwlock = RWLock()
+        
         self.ros_setup()
+    
+    
+    def command_mobile_base_velocity_and_publish_state(self):
+        self.robot_mode_rwlock.acquire_read()
+        
+
+        current_clock = self.get_clock().now()  # update ros time
+        current_time = current_clock.to_msg()
+
+        
+        # TODO: pull robot status and publish joint_state
+        robot_status = self.robot_sim._pull_status()      # update with ros status
+        # robot_status = self.status.copy()     # update with mujoco status
+        joint_state = JointState()
+        joint_state.header.stamp = current_time
+        joint_state.name = self.robot_sim.urdf_model.joints_names
+        
+        
+        joint_state.position = positions
+        joint_state.velocity = velocities
+        joint_state.effort = efforts
+        self.joint_state_pub.publish(joint_state)
+        
+        # TODO: pull camera data and publish images
+        camera_data = self.robot_sim.pull_camera_data()
+        
         
     def ros_setup(self):
         self.node_name = self.get_name()
         
+        
+        self.mutex_group = MutuallyExclusiveCallbackGroup() # only one callback can be executing
+        
+        self.declare_parameter('rate', 30.0)
+        
+        self.joint_state_pub = self.create_publisher(JointState, 'joint_states', 1)
+        self.joint_limits_pub = self.create_publisher(JointState, 'joint_limits', 1)
+        
+        
+        timer_period = 1.0 / self.joint_state_rate
+        self.timer = self.create_timer(timer_period, self.command_mobile_base_velocity_and_publish_state, callback_group=self.mutex_group)
