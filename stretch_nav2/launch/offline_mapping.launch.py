1b0ee5b560da44015333c9f568b80ba90467b0ab
Binit Shah <bshah@hello-robot.com>
2025-08-05 16:20:48 -0400

    Clean up offline_mapping.launch.py

--- stretch_nav2/launch/offline_mapping.launch.py
+++ stretch_nav2/launch/offline_mapping.launch.py
@@ -10,14 +10,10 @@ from ament_index_python.packages import get_package_share_directory, get_package
 
 def generate_launch_description():
     stretch_core_path = get_package_share_directory('stretch_core')
-    stretch_navigation_path = get_package_share_directory('stretch_nav2')
     navigation_package = str(get_package_share_path("stretch_nav2"))
 
     rviz_param = DeclareLaunchArgument('use_rviz', default_value='true', choices=['true', 'false'])
-    
-    teleop_type = DeclareLaunchArgument(
-        'teleop_type', default_value="joystick", description="how to teleop ('keyboard', 'joystick' or 'none')")
-    
+
     declare_use_sim_time_argument = DeclareLaunchArgument(
         'use_sim_time',
         default_value='false',
@@ -32,27 +28,25 @@ def generate_launch_description():
 
     rviz_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([get_package_share_directory('nav2_bringup'), '/launch/rviz_launch.py']),
-        condition=IfCondition(LaunchConfiguration('use_rviz')))    
+        condition=IfCondition(LaunchConfiguration('use_rviz')))
+
+    slam_toolbox = Node(
+        package='slam_toolbox',
+        executable='sync_slam_toolbox_node',
+        name='slam_toolbox',
+        output='screen',
+        parameters=[
+            PathJoinSubstitution([navigation_package, 'config', 'mapper_params_online_async.yaml']),
+        ]
+    )
 
     ld = LaunchDescription([
         rviz_param,
-        teleop_type,
         declare_use_sim_time_argument,
         stretch_driver_launch,
         rplidar_launch,
         rviz_launch,
+        slam_toolbox,
     ])
-
-    ld.add_action(
-        Node(
-            package='slam_toolbox',
-            executable='sync_slam_toolbox_node',
-            name='slam_toolbox',
-            output='screen',
-            parameters=[
-                PathJoinSubstitution([navigation_package, 'config', 'mapper_params_online_async.yaml']),
-            ]
-        )
-    )
 
     return ld
