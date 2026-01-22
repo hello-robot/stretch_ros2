d574b7c227ded99623fce16ad8dd60f89be58894
Jason Koubi <jasonkoubi@hello-robot.com>
2026-01-21 16:09:50 -0800

    include cython_min_cost_path .so in package data

--- stretch_funmap/setup.py
+++ stretch_funmap/setup.py
@@ -7,6 +7,10 @@ setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(),
+    include_package_data=True,  
+    package_data={              
+        package_name: ['cython_min_cost_path*.so'],
+    },
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
